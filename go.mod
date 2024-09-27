e3ca19c1d1ede37af69395c570c09255925f26fe
Caleb Gardner <calebq23@gmail.com>
2024-09-27 05:25:43 -0500

    Reset to zero

--- go.mod
+++ go.mod
@@ -1,26 +1,3 @@
 module github.com/CalebQ42/LinuxPA
 
-go 1.17
-
-require (
-	github.com/CalebQ42/GoAppImage v0.4.0
-	github.com/gotk3/gotk3 v0.6.1
-	github.com/mholt/archiver/v3 v3.5.1
-	github.com/pkg/browser v0.0.0-20210911075715-681adbf594b8
-)
-
-require (
-	github.com/adrg/xdg v0.2.2 // indirect
-	github.com/andybalholm/brotli v1.0.1 // indirect
-	github.com/dsnet/compress v0.0.2-0.20210315054119-f66993602bf5 // indirect
-	github.com/golang/snappy v0.0.2 // indirect
-	github.com/klauspost/compress v1.11.4 // indirect
-	github.com/klauspost/pgzip v1.2.5 // indirect
-	github.com/nwaples/rardecode v1.1.0 // indirect
-	github.com/pierrec/lz4/v4 v4.1.2 // indirect
-	github.com/ulikunitz/xz v0.5.9 // indirect
-	github.com/xi2/xz v0.0.0-20171230120015-48954b6210f8 // indirect
-	go.lsp.dev/uri v0.3.0 // indirect
-	golang.org/x/sys v0.0.0-20210616045830-e2b7044e8c71 // indirect
-	gopkg.in/ini.v1 v1.62.0 // indirect
-)
+go 1.23.1
--- main.go
+++ main.go
@@ -1,61 +1,8 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"os"
-
-	"github.com/gotk3/gotk3/gtk"
-)
-
 const (
-	version = "2.2.0.0"
-)
-
-var (
-	master    map[string][]app
-	linmaster map[string][]app
-	cats      []string
-	lin       []string
-	comEnbld  bool
-	populated bool
+	version = "3.0.0.0"
 )
 
 func main() {
-	forced := flag.Bool("force-update", false, "Force the update dialog to be shown")
-	flag.Parse()
-	os.MkdirAll("PortableApps/LinuxPACom", 0777)
-	master = make(map[string][]app)
-	linmaster = make(map[string][]app)
-	uiStart(*forced)
-}
-
-func uiStart(forced bool) {
-	gtk.Init(nil)
-	setup()
-	win, err := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
-	if err != nil {
-		fmt.Println("Window not created", err)
-	}
-	win.SetTitle("LinuxPA")
-	win.Connect("destroy", func() {
-		savePrefs()
-		gtk.MainQuit()
-	})
-	win.SetDefaultSize(500, 500)
-	win.SetPosition(gtk.WIN_POS_CENTER)
-	ui(win)
-	win.ShowAll()
-	win.Show()
-	update(win, forced)
-	gtk.Main()
-}
-
-func contains(arr []string, str string) bool {
-	for _, v := range arr {
-		if v == str {
-			return true
-		}
-	}
-	return false
 }
